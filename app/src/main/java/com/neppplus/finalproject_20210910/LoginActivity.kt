8ec03387671fa3f5e3fb733b575e8c54e9b56e8a
조경진 <cho881020@gmail.com>
2021-09-10 10:33:36 +0900

    액티비티 이름 변경

--- app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
@@ -1,17 +1,16 @@
 package com.neppplus.finalproject_20210910
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.neppplus.finalproject_20210910.databinding.ActivityMainBinding
+import com.neppplus.finalproject_20210910.databinding.ActivityLoginBinding
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
 
