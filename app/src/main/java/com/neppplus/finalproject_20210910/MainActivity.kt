308757152126c452d66e1e8caab941e063104eec
조경진 <cho881020@gmail.com>
2021-09-10 09:46:19 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.neppplus.finalproject_20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neppplus.finalproject_20210910.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
