caeaf50b118271b835c5a1295a7b1355b89dadb1
조경진 <cho881020@gmail.com>
2021-09-10 11:59:18 +0900

    페북로그인 버튼 활용 - 로그인 처리

--- app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.finalproject_20210910
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.finalproject_20210910.databinding.ActivityLoginBinding
@@ -8,12 +9,27 @@ import android.content.pm.PackageManager
 import android.content.pm.PackageInfo
 import android.util.Base64
 import android.util.Log
+import com.facebook.CallbackManager
 import java.security.MessageDigest
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+
+import android.view.View
+
+import com.facebook.login.widget.LoginButton
+
+
+
 
 
 class LoginActivity : BaseActivity() {
 
     lateinit var binding : ActivityLoginBinding
+
+    lateinit var callbackManager : CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +40,29 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        callbackManager = CallbackManager.Factory.create();
+
+        binding.loginButton.setReadPermissions("email")
+
+
+
+        // Callback registration
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+
+                Log.d("확인용", loginResult.toString())
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })
 
     }
 
@@ -40,4 +79,10 @@ class LoginActivity : BaseActivity() {
             Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
