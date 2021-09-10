868628761fc431fe3376a570801d2189a1bfd970
조경진 <cho881020@gmail.com>
2021-09-10 11:46:18 +0900

    로그인화면 - KeyHash값 추출  - 페이스북 라이브러리 설치 진행중

--- app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/LoginActivity.kt
@@ -3,6 +3,13 @@ package com.neppplus.finalproject_20210910
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.finalproject_20210910.databinding.ActivityLoginBinding
+import android.content.pm.PackageManager
+
+import android.content.pm.PackageInfo
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
+
 
 class LoginActivity : BaseActivity() {
 
@@ -22,5 +29,15 @@ class LoginActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        카톡으로 받은 코드 복붙 => 키 해시값 추출
+        val info = packageManager.getPackageInfo(
+            "com.neppplus.finalproject_20210910",
+            PackageManager.GET_SIGNATURES
+        )
+        for (signature in info.signatures) {
+            val md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
     }
 }
