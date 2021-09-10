d27ace8a87d542d4b9636dbf5682f2ccf0fdfaa2
조경진 <cho881020@gmail.com>
2021-09-10 10:00:17 +0900

    BaseActivity 세팅까지 진행

--- app/src/main/java/com/neppplus/finalproject_20210910/MainActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/MainActivity.kt
@@ -5,15 +5,23 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.finalproject_20210910.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
 
-        binding.textTxt.text = "데이터 바인딩 실험"
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
 
     }
 }
