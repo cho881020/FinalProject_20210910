bf1e1960229cf16a7562c72c7f579e7ba96f8289
조경진 <cho881020@gmail.com>
2021-09-13 15:55:02 +0900

    어댑터 생성 / 리스트뷰와 연결

--- app/src/main/java/com/neppplus/finalproject_20210910/MainActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_20210910.adapters.AppointmentAdapter
 import com.neppplus.finalproject_20210910.databinding.ActivityMainBinding
 import com.neppplus.finalproject_20210910.datas.AppointmentData
 import com.neppplus.finalproject_20210910.datas.BasicResponse
@@ -19,6 +20,7 @@ class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
 
     val mAppointmentList = ArrayList<AppointmentData>()
+    lateinit var mAdapter : AppointmentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +43,9 @@ class MainActivity : BaseActivity() {
         Toast.makeText(mContext, "${GlobalData.loginUser!!.nickName}님 환영합니다!", Toast.LENGTH_SHORT).show()
 
         getAppointmentListFromServer()
+
+        mAdapter = AppointmentAdapter(mContext, R.layout.appointment_list_item, mAppointmentList)
+        binding.appointmentListView.adapter = mAdapter
     }
 
     fun getAppointmentListFromServer() {
@@ -54,6 +59,7 @@ class MainActivity : BaseActivity() {
                 mAppointmentList.addAll( basicResponse.data.appointments )
 
 //                어댑터 새로고침
+                mAdapter.notifyDataSetChanged()
 
             }
 
