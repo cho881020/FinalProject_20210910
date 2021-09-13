bfa7efa9502be05266ae290dba23b0e636c573a5
조경진 <cho881020@gmail.com>
2021-09-13 11:13:56 +0900

    바인딩처리 - 입력한 일시 / 장소 받아내기 필요함

--- app/src/main/java/com/neppplus/finalproject_20210910/EditAppoinmentActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/EditAppoinmentActivity.kt
@@ -2,10 +2,44 @@ package com.neppplus.finalproject_20210910
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_20210910.databinding.ActivityEditAppoinmentBinding
 
-class EditAppoinmentActivity : AppCompatActivity() {
+class EditAppoinmentActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditAppoinmentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_appoinment)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_appoinment)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+//        확인버튼이 눌리면?
+
+        binding.okBtn.setOnClickListener {
+
+//            입력한 값들 받아오기
+//            1. 일정 제목
+            val inputTitle = binding.titleEdt.text.toString()
+
+//            2. 약속 일시? -> "2021-09-13 11:11" String 변환까지.
+
+//            3. 약속 장소?
+//            - 장소 이름
+            val inputPlaceName = binding.placeSearchEdt.text.toString()
+
+//            - 장소 위도/경도?
+
+        }
+
+
+    }
+
+    override fun setValues() {
+
     }
 }
