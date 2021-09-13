24361978678793bde66ad2a5233caf9e6fe8b89c
조경진 <cho881020@gmail.com>
2021-09-13 11:05:40 +0900

    약속 등록화면 초안 UI 작업

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/EditAppoinmentActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.finalproject_20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAppoinmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_appoinment)
+    }
+}
