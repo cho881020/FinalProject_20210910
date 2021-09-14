943cbedeaf8c41514cf407c2c69a5731d5551811
조경진 <cho881020@gmail.com>
2021-09-14 11:09:55 +0900

    지도보기 화면 => 받아낸 약속정보를 갖고  - 실제 지도 카메라 이동  - 마커도 실제로 찍어주기

--- app/src/main/java/com/neppplus/finalproject_20210910/datas/UserData.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/datas/UserData.kt
@@ -1,11 +1,12 @@
 package com.neppplus.finalproject_20210910.datas
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 class UserData(
     var id: Int,
     var provider: String,
     @SerializedName("nick_name")
     var nickName: String,
-    var email: String) {
+    var email: String) : Serializable {
 }
