e595d49044e827a329626753e30594d131cfc8c9
조경진 <cho881020@gmail.com>
2021-09-14 15:37:56 +0900

    사용자 데이터에 준비 소요 분 데이터 추가  - 프로필 화면에서 보여주기

--- app/src/main/java/com/neppplus/finalproject_20210910/MySettingActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/MySettingActivity.kt
@@ -8,16 +8,26 @@ import com.neppplus.finalproject_20210910.utils.GlobalData
 
 class MySettingActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMySettingBinding
+    lateinit var binding: ActivityMySettingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =  DataBindingUtil.setContentView(this, R.layout.activity_my_setting)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_setting)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.readyTimeLayout.setOnClickListener {
+
+//            응용문제 => AlertDialog로 준비시간을 입력받자.
+//             EditText를 사용할 수 있는 방법? 구글링
+
+
+
+
+        }
 
     }
 
@@ -25,7 +35,18 @@ class MySettingActivity : BaseActivity() {
 
         titleTxt.text = "내 정보 설정"
 
-        binding.nicknameTxt.text =  GlobalData.loginUser!!.nickName
+        binding.nicknameTxt.text = GlobalData.loginUser!!.nickName
+
+//       로그인한사람의 준비시간이 1시간 이상 or 아니냐
+        if (GlobalData.loginUser!!.readyMinute >= 60) {
+            val hour = GlobalData.loginUser!!.readyMinute / 60
+            val minute = GlobalData.loginUser!!.readyMinute % 60
+
+            binding.readyTimeTxt.text = "${hour}시간 ${minute}분"
+        }
+        else {
+            binding.readyTimeTxt.text = "${GlobalData.loginUser!!.readyMinute}분"
+        }
 
     }
 }
