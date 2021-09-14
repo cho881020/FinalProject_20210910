c264eb34a06be9fd63dbe971da74c3f8a78e4e28
조경진 <cho881020@gmail.com>
2021-09-14 18:05:35 +0900

    데이터바인딩 세팅

--- app/src/main/java/com/neppplus/finalproject_20210910/EditMyPlaceActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/EditMyPlaceActivity.kt
@@ -2,11 +2,16 @@ package com.neppplus.finalproject_20210910
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_20210910.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditMyPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_my_place)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_my_place)
         setupEvents()
         setValues()
     }
@@ -16,6 +21,8 @@ class EditMyPlaceActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        titleTxt.text = "내 출발장소 추가"
 
     }
 }
