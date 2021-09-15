d45cbf1f83f572554d78290b4bf7ec9263118413
조경진 <cho881020@gmail.com>
2021-09-15 11:05:09 +0900

    약속어댑터 -> 리싸이클러뷰 어댑터로 추가

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/adapters/AppointmentRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.neppplus.finalproject_20210910.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.finalproject_20210910.R
+import com.neppplus.finalproject_20210910.datas.AppointmentData
+
+class AppointmentRecyclerAdapter(
+    val mContext: Context,
+    val mList:List<AppointmentData>) : RecyclerView.Adapter<AppointmentRecyclerAdapter.AppointmentViewHolder>() {
+
+    class AppointmentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val titleTxt = view.findViewById<TextView>(R.id.titleTxt)
+        val dateTimeTxt = view.findViewById<TextView>(R.id.dateTimeTxt)
+        val placeNameTxt = view.findViewById<TextView>(R.id.placeNameTxt)
+        val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppointmentViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item, parent, false)
+        return AppointmentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AppointmentViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
+
+}
