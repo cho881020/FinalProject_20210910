915c76a54ae8549c4c51322a9eadc44b38837276
조경진 <cho881020@gmail.com>
2021-09-16 11:58:30 +0900

    친구 목록 표시 준비중

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/ViewMyFriendsListActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.finalproject_20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMyFriendsListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_my_friends_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
