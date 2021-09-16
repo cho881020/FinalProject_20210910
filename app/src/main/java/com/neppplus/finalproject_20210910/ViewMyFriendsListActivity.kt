4c39b0c00570a6c563c2a9569c664ca938ada398
조경진 <cho881020@gmail.com>
2021-09-16 12:19:32 +0900

    프래그먼트에 데이터바인딩 처리

--- app/src/main/java/com/neppplus/finalproject_20210910/ViewMyFriendsListActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/ViewMyFriendsListActivity.kt
@@ -2,11 +2,16 @@ package com.neppplus.finalproject_20210910
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_20210910.databinding.ActivityViewMyFriendsListBinding
 
 class ViewMyFriendsListActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewMyFriendsListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_my_friends_list)
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_view_my_friends_list)
         setupEvents()
         setValues()
     }
--- app/src/main/java/com/neppplus/finalproject_20210910/fragments/MyFriendsListFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/fragments/MyFriendsListFragment.kt
@@ -4,22 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.neppplus.finalproject_20210910.R
+import com.neppplus.finalproject_20210910.databinding.FragmentMyFriendsListBinding
 
 class MyFriendsListFragment : Fragment() {
+
+    lateinit var binding: FragmentMyFriendsListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return  inflater.inflate(R.layout.fragment_my_friends_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_friends_list, container, false)
+        return  binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
 
 
     }
