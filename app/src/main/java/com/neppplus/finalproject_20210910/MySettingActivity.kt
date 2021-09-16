158ee6c4ea40465aa3a88cf085801c659e21d057
조경진 <cho881020@gmail.com>
2021-09-16 18:20:39 +0900

    갤러리로 가는 Intent 변경   - 사진 첨부 기능 적용 (프사 변경 실제 진행)

--- app/src/main/java/com/neppplus/finalproject_20210910/MySettingActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/MySettingActivity.kt
@@ -92,9 +92,10 @@ class MySettingActivity : BaseActivity() {
 //                    갤러리로 사진을 가지러 이동. (추가 작업)
 
                     val myIntent = Intent()
-                    myIntent.action = Intent.ACTION_GET_CONTENT
+                    myIntent.action = Intent.ACTION_PICK
                     myIntent.type = "image/*"
-                    startActivityForResult(Intent.createChooser(myIntent, "프사 선택하기"),  REQ_FOR_GALLERY)
+                    myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+                    startActivityForResult(myIntent,  REQ_FOR_GALLERY)
 
                 }
 
