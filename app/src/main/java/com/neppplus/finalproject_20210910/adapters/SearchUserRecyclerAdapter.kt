d22cbb3fe6d2d648cf6ef1cc46c31afa02a65cb2
조경진 <cho881020@gmail.com>
2021-09-16 16:43:11 +0900

    친구 검색결과 -> 친구 추가 버튼 이벤트처리  -> API에 친구 추가 요청 해야함

--- app/src/main/java/com/neppplus/finalproject_20210910/adapters/SearchUserRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/adapters/SearchUserRecyclerAdapter.kt
@@ -1,11 +1,14 @@
 package com.neppplus.finalproject_20210910.adapters
 
 import android.content.Context
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.neppplus.finalproject_20210910.R
@@ -20,6 +23,7 @@ class SearchUserRecyclerAdapter(
         val profileImg = view.findViewById<ImageView>(R.id.profileImg)
         val nicknameTxt = view.findViewById<TextView>(R.id.nicknameTxt)
         val socialLoginImg = view.findViewById<ImageView>(R.id.socialLoginImg)
+        val addFriendBtn = view.findViewById<Button>(R.id.addFriendBtn)
 
         fun bind(context: Context, data: UserData) {
             Glide.with(context).load(data.profileImgURL).into(profileImg)
@@ -37,6 +41,19 @@ class SearchUserRecyclerAdapter(
                     socialLoginImg.visibility = View.GONE
                 }
             }
+
+//            친구추가 버튼 눌림 이벤트 처리
+            addFriendBtn.setOnClickListener {
+//                ~~님을 친구로 추가하겠습니까?
+                val alert = AlertDialog.Builder(context)
+                alert.setMessage("${data.nickName}님을 친구로 추가하겠습니까?")
+                alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                })
+                alert.setNegativeButton("취소", null)
+                alert.show()
+            }
+
         }
 
     }
