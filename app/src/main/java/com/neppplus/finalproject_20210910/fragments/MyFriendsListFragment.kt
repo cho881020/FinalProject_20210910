563d8a61b11c57600143bbac63f480e7e3b24643
조경진 <cho881020@gmail.com>
2021-09-16 12:14:59 +0900

    내 친구목록 프래그먼트 생성

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/fragments/MyFriendsListFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.finalproject_20210910.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.finalproject_20210910.R
+
+class MyFriendsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.fragment_my_friends_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+
+}
