86fdbefcf61674abce6e5528d154228ed80bb7b0
조경진 <cho881020@gmail.com>
2021-09-17 14:37:10 +0900

    약속 정보 확인용 액티비티 추가

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/ViewAppointmentDetailActivity.kt
@@ -0,0 +1,26 @@
+package com.neppplus.finalproject_20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_20210910.databinding.ActivityViewAppointmentDetailBinding
+
+class ViewAppointmentDetailActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewAppointmentDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_appointment_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
