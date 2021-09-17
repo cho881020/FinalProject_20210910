86f2cf87ef75ab438655e8e37afb8573d044c359
조경진 <cho881020@gmail.com>
2021-09-17 15:49:45 +0900

    네이버 지도 객체 얻기 + 마커/카메라 세팅

--- app/src/main/java/com/neppplus/finalproject_20210910/ViewAppointmentDetailActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/ViewAppointmentDetailActivity.kt
@@ -3,6 +3,10 @@ package com.neppplus.finalproject_20210910
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 import com.neppplus.finalproject_20210910.databinding.ActivityViewAppointmentDetailBinding
 import com.neppplus.finalproject_20210910.datas.AppointmentData
 import java.text.SimpleDateFormat
@@ -43,8 +47,7 @@ class ViewAppointmentDetailActivity : BaseActivity() {
         binding.timeTxt.text =  sdf.format(mAppointmentData.datetime)
 
 //        3) 도착지 좌표 지도에 설정
-//          - 마커를 하나 생성 => 좌표에 찍어주기
-//          - 카메라 이동 => 도착지 좌표로 카메라 이동.
+        setNaverMap()
 
 //        4) 응용 1 - 친구목록 => 레이아웃에 xml inflate해서 하나씩 addView
 //        5) 응용 2 - 출발지 좌표도 지도에 설정.
@@ -54,5 +57,35 @@ class ViewAppointmentDetailActivity : BaseActivity() {
 
 
     }
+
+    fun setNaverMap() {
+//        지도 관련 코드.
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.naverMapFrag) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.naverMapFrag, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+            val naverMap = it
+
+
+//          - 마커를 하나 생성 => 도착지 좌표에 찍어주기
+
+            val dest = LatLng(mAppointmentData.latitude, mAppointmentData.longitude)
+
+            val marker = Marker()
+            marker.position = dest
+            marker.map = naverMap
+
+//          - 카메라 이동 => 도착지 좌표로 카메라 이동.
+            val cameraUpdate = CameraUpdate.scrollTo(dest)
+            naverMap.moveCamera(cameraUpdate)
+
+        }
+
+    }
+
 
 }
