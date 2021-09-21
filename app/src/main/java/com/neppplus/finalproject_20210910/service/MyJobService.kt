69508266ca48d2178586c5f69e8db70bf1818a90
조경진 <cho881020@gmail.com>
2021-09-21 14:05:24 +0900

    기본 알람까지

--- app/src/main/java/com/neppplus/finalproject_20210910/service/MyJobService.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/service/MyJobService.kt
@@ -1,7 +1,11 @@
 package com.neppplus.finalproject_20210910.service
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.SystemClock
 import android.util.Log
 
 class MyJobService : JobService() {
@@ -13,6 +17,20 @@ class MyJobService : JobService() {
     override fun onStartJob(p0: JobParameters?): Boolean {
 
         Log.d("테스트", p0!!.jobId.toString())
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)  // 1
+        val pendingIntent = PendingIntent.getBroadcast(     // 2
+            this, AlarmReceiver.NOTIFICATION_ID, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+        val triggerTime = (SystemClock.elapsedRealtime()  // 4
+                + 60 * 1000)
+        alarmManager.set(   // 5
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            triggerTime,
+            pendingIntent
+        )
         return false
     }
 
