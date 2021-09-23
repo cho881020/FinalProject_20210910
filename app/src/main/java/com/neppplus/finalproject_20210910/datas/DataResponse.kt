e1103de952b38ad3b441e130881db90afba42206
조경진 <cho881020@gmail.com>
2021-09-23 17:39:01 +0900

    초대받은 약속 목록 표시

--- app/src/main/java/com/neppplus/finalproject_20210910/datas/DataResponse.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/datas/DataResponse.kt
@@ -1,11 +1,15 @@
 package com.neppplus.finalproject_20210910.datas
 
+import com.google.gson.annotations.SerializedName
+
 class DataResponse(
 //    로그인 성공시 파싱용 변수.
     var user: UserData,
     var token: String,
 //    이 밑으로는 약속 목록파싱용 변수.
     var appointments: List<AppointmentData>,
+    @SerializedName("invited_appointments")
+    var invitedAppointments: List<AppointmentData>,
 //    장소 목록
     var places: List<PlaceData>,
 //    친구목록
--- app/src/main/java/com/neppplus/finalproject_20210910/fragments/InvitedAppointmentsListFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/fragments/InvitedAppointmentsListFragment.kt
@@ -9,10 +9,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.finalproject_20210910.R
+import com.neppplus.finalproject_20210910.adapters.AppointmentRecyclerAdapter
 import com.neppplus.finalproject_20210910.adapters.MyFriendsRecyclerAdapter
 import com.neppplus.finalproject_20210910.databinding.FragmentInvitedAppointmentsListBinding
 import com.neppplus.finalproject_20210910.databinding.FragmentMyAppointmentsListBinding
 import com.neppplus.finalproject_20210910.databinding.FragmentMyFriendsListBinding
+import com.neppplus.finalproject_20210910.datas.AppointmentData
 import com.neppplus.finalproject_20210910.datas.BasicResponse
 import com.neppplus.finalproject_20210910.datas.UserData
 import retrofit2.Call
@@ -33,6 +35,11 @@ class InvitedAppointmentsListFragment : BaseFragment() {
     }
 
     lateinit var binding: FragmentInvitedAppointmentsListBinding
+
+    val mAppointmentList = ArrayList<AppointmentData>()
+//    lateinit var mAdapter : AppointmentAdapter
+
+    lateinit var mRecyclerAdapter : AppointmentRecyclerAdapter
 
 
     override fun onCreateView(
@@ -56,14 +63,42 @@ class InvitedAppointmentsListFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        mRecyclerAdapter = AppointmentRecyclerAdapter(mContext, mAppointmentList)
+        binding.invitedAppointmentsRecyclerView.adapter = mRecyclerAdapter
 
+        binding.invitedAppointmentsRecyclerView.layoutManager = LinearLayoutManager(mContext)
 
     }
+
 
     override fun onResume() {
         super.onResume()
+        getAppointmentListFromServer()
     }
 
+    fun getAppointmentListFromServer() {
 
+        apiService.getRequestAppointmentList().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                val basicResponse = response.body()!!
+
+                mAppointmentList.clear()
+
+//                약속목록변수에 => 서버가 알려준 약속목록을 전부 추가.
+                mAppointmentList.addAll( basicResponse.data.invitedAppointments )
+
+//                어댑터 새로고침
+                mRecyclerAdapter.notifyDataSetChanged()
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
 
 }
