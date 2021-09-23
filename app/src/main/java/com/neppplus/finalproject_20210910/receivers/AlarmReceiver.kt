f187fe18ed05c2ad7434ff4ff0367d7f13a2e067
조경진 <cho881020@gmail.com>
2021-09-23 11:58:31 +0900

    약속등록후 1분 후에 -> 알람을 등록 (1분 후)  => 총 2분 후에 알람이 울리는지 확인

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/receivers/AlarmReceiver.kt
@@ -0,0 +1,19 @@
+package com.neppplus.finalproject_20210910.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        val ALARM_ID = 1001
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        
+        Log.d("알람울림", "테스트로그")
+        
+    }
+}
--- app/src/main/java/com/neppplus/finalproject_20210910/services/MyJobService.kt
+++ app/src/main/java/com/neppplus/finalproject_20210910/services/MyJobService.kt
@@ -1,8 +1,13 @@
 package com.neppplus.finalproject_20210910.services
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
 import android.util.Log
+import com.neppplus.finalproject_20210910.receivers.AlarmReceiver
+import java.util.*
 
 class MyJobService : JobService() {
 
@@ -16,6 +21,31 @@ class MyJobService : JobService() {
     override fun onStartJob(p0: JobParameters?): Boolean {
 
         Log.d("예약작업시작", p0!!.jobId.toString())
+
+//        임시 : 1분 후에 알람 설정.
+//        실제 : 약속시간 - (API 에서 알려준) 교통 소요시간 - 내 준비시간  계산된 시간에 알람.
+
+//        알람을 울리게 도와주는 도구. => Broadcast 송신.
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+//        실제로 알람이 울리면 실행할 코드 =>  BroadcastReceiver에 작업 해둘 필요 있다.
+        val myIntent = Intent(this, AlarmReceiver::class.java)
+
+//        할 일을 가지고 대기(Pending) 해주는 Intent.
+
+        val pendingIntent = PendingIntent.getBroadcast(this,
+            AlarmReceiver.ALARM_ID,
+            myIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+//        알람이 울릴 시간 설정 (임시 : 1분 후)
+        val now = Calendar.getInstance()
+        val triggerTime = now.timeInMillis + 60 * 1000
+
+//        실제 알람 기능 설정.
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, pendingIntent)
+
 
         return false
     }
