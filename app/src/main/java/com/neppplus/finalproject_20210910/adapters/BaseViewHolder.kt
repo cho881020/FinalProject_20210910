c81e3369a6dbcec36837634f649d7818f5ded43f
조경진 <cho881020@gmail.com>
2021-09-29 18:09:29 +0900

    BaseViewHolder 클래스 생성   - 폰트 일괄 설정 기능 내장

---
+++ app/src/main/java/com/neppplus/finalproject_20210910/adapters/BaseViewHolder.kt
@@ -0,0 +1,12 @@
+package com.neppplus.finalproject_20210910.adapters
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.finalproject_20210910.utils.FontChanger
+
+class BaseViewHolder(context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
+    init {
+        FontChanger.setGlobalFont(context, itemView)
+    }
+}
